80ac37fa0fbe5abacb8be56bf797bcc4387dc894
WellinGtoMartins <wellingtonoli.martins@gmail.com>
2023-09-11 22:21:37 -0300

    Initial commit

---
+++ src/predicate/PredicateExample.java
@@ -0,0 +1,30 @@
+package predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Representa uma função que aceita um argumento do tipo T e retorna um valor booleano
+ * É comumente usada para filtrar os elementos do Stream com base em alguma condição.
+ */
+
+public class PredicateExample {
+    public static void main(String[] args) {
+        // Criar um lista de palavras
+        List<String> palavras = Arrays.asList("Java", "Kotlin", "Python", "JavaScript", "C", "GO", "Ruby");
+
+        // Criar um Predicate que verifica se a palavra tem mais de 5 caracteres
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+        // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
+        // imprimir cada palavra que passou no filtro
+        palavras.stream().filter(s -> s.length() > 5)
+                .forEach(System.out::println);
+
+
+    }
+
+
+
+}
